a6c1c74f15ba03c476cf648079bd36811c57c117
defghij <defghij@arda>
2025-04-13 13:51:55 -0400

    add dashboard with ascii art

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -185,12 +185,51 @@ local plugins = {
     config = get_setup("dooing"),
   },
 
-  ---- Splash Page for Startup
-  --{
-    --'nvimdev/dashboard-nvim',
-    --event = 'VimEnter',
-    --dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  --}
+  -- Splash Page for Startup
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        config = {
+          header = {
+            "                                __ _._.,._.__             ",
+            "                          .oWWWWWWWWWWWWWWWWP'            ",
+            "                        .dWWWWWWWWWWWWWWWWWK              ",
+            "          ,W`            8WWWWWWWWWWWWWWWWWWWWW#>._       ",
+            "         :WWb           8WWWWWWWWWWWWWWWWWWWWWWWWWWb.     ",
+            "          `YWb          8WWWWWWWWWWWWWWWWWWWWWWWWWWWWb.   ",
+            "            `Yb.       dWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWb  ",
+            "              `Yb.___.WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWb",
+            "                `YWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWP' ",
+            "                  `WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWP''   ",
+            " 'Y888K    'Y8P''Y888YWWWWWWWWWWWWWWWWWWWW8o._            ",    
+            "   88888b    8    8888`YWWWWWWWWWWWWWWWWWWWWWWW8o.        ",
+            "   8'Y8888b  8    8888   YWWWWWWWWWWWWWWWWWWWWWWWWo,      ",
+            "   8  'Y8888b8    8888''Y8`YWWWWWWWWWWWWWWWWWWWWWWb.      ",
+            "   8    'Y8888    8888   Y  `YWWWWWWWWWWWWWWWWWWWWWh      ",
+            "   8      'Y88    8888     .d `YWWWWWWWWWWWWWWWWWWWWb     ",
+            " .d8b.      '8  .d8888b..d88P   `YWWWWWWWWWWWWWWWWWWW8    ",
+            "                                  `YWWWWWWWWWWWWWWWWWb.   ",
+            ".______.           Y888P''Y8b. 'Y888YWWWWWWWWWWWWWWWW8    ",
+            "| 全 神|            888    888   Y888`YWWWWWWWWWWWWWWK    ",
+            "| て は|            888   d88P    Y88b `YWWWWWWWWWWWW8    ",
+            "| 世 天|            888'Y88K'      Y88b dPYWWWWWWWWWWP    ",
+            "| は に|            888  Y88b       Y88dP  `YWWWWWWWWb    ",
+            "| 事 在|            888   Y88b       Y8P     `YWWWWWWW    ",
+            "| も り|          .d888b.  Y88b.      Y        `YWWWWW    ",
+            "| な   |                                         `YWWK    ",
+            "| し   |                                           `Y8    ",
+            "'------'                                             '    ",
+            "",
+            "",
+          },
+          footer = { "God is in His heaven, all is right with the world" },
+        }    
+      }
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  }
 }
 
 local opts = {}
